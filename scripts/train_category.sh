3b3e4394b5bd447ebb2a6523035736edff19a56c
Edward Hui <ka.hei.hui@autodesk.com>
2025-04-11 13:59:50 -0400

    1. update code for octfusion data

--- scripts/train_category.sh
+++ scripts/train_category.sh
@@ -1,4 +1,4 @@
 # please modify the sdf_folder to your own path
 
 # singe category
-python train.py --data_class Thingi10K --name Thingi10K --batch_size 16 --new True --continue_training False --image_size 64 --training_epoch 4000 --ema_rate 0.999 --base_channels 32 --save_last False --save_every_epoch 200 --with_attention True --use_text_condition False --use_sketch_condition False --split_dataset True  --lr 1e-4 --optimizier adamw --sdf_folder data/Thingi10K/sdf
+python train.py --data_class Thingi10K --name Thingi10K --batch_size 16 --new True --continue_training False --image_size 64 --training_epoch 4000 --ema_rate 0.999 --base_channels 32 --save_last False --save_every_epoch 200 --with_attention True --use_text_condition False --use_sketch_condition False --split_dataset True  --lr 1e-4 --optimizier adamw --sdf_folder /data/octfusion_data/Thingi10K/sdf
